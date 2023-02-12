85daf60a773c753ed4aaa18f68a16b0e7447eeeb
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-02-12 18:14:23 -0300

    uint tested

--- lib/uint/code.c
+++ lib/uint/code.c
@@ -7,11 +7,6 @@ luint uint_add(uint a, uint b)
     return (luint)a + (luint)b;
 }
 
-luint uint_add_3(uint a, uint b, uint c) 
-{
-    return (luint)a + (luint)b + (luint)c;
-}
-
 luint uint_mul(uint a, uint b) 
 {
     return (luint)a * (luint)b;
---
+++ lib/uint/test/test.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
+
+#include "../code.c"
+
+void test_dec()
+{
+    printf("\n\ttest_dec");
+
+    luint value = 0;
+    assert(DECH(value) == 0);
+    assert(DECL(value) == 0);
+
+    value = 0x100000002;
+    assert(DECH(value) == 1);
+    assert(DECL(value) == 2);
+}
+
+void test_add()
+{
+    printf("\n\ttest_add");
+
+    luint res = uint_add(0, 0);
+    assert(res == 0);
+
+    res = uint_add(0, 5);
+    assert(res == 5);
+
+    res = uint_add(UINT_MAX, 3);
+    assert(res == 0x100000002);
+
+    res = uint_add(UINT_MAX, UINT_MAX);
+    assert(res == 0x1FFFFFFFE);
+}
+
+void test_mul()
+{
+    printf("\n\ttest_mul");
+
+    luint res = uint_mul(2, 2);
+    assert(res == 4);
+
+    res = uint_mul(0, 5);
+    assert(res == 0);
+
+    res = uint_mul(UINT_MAX, 2);
+    assert(res == 0x1FFFFFFFE);
+
+    res = uint_mul(UINT_MAX, UINT_MAX);
+    assert(res == 0xFFFFFFFE00000001);
+}
+
+void test_uint()
+{
+    printf("\ntest uint");
+
+    test_dec();
+    test_add();
+    test_mul();
+}
+
+int main() {
+    setbuf(stdout, NULL);
+    test_uint();
+    printf("\n\n\t\tTest successful\n\n");
+    return 0;
+}
