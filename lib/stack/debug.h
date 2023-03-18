8b5a1c29a41dcbee6b70ee7b0b695ce3a07c26a7
Renan Souza <renan.rodrigues.souza1@gmail.com>
2023-03-18 20:15:23 -0300

    stack tests (#11)

--- lib/stack/debug.h
+++ lib/stack/debug.h
@@ -5,6 +5,9 @@
 
 #ifdef DEBUG
 
+#define STACK_UINT(S, UINT) \
+    assert(stack_push(&S, BYTES32_UINT(UINT)))
+
 void stack_display(stack_t s);
 bool stack_memory();
 
--- lib/stack/test/test.c
+++ lib/stack/test/test.c
@@ -4,30 +4,30 @@
 #include "../debug.h"
 #include "../../bytes/bytes32/debug.h"
 
+
+
 void test_push()
 {
     printf("\n\ttest push\t\t");
 
     stack_t s = stack_init();
-    assert(stack_push(&s, b_one));
+    STACK_UINT(s, 1);
     assert(s.count == 1);
     ASSERT_BYTES32_UINT(s.b[0], 1);
     stack_free(s);
 
     s = stack_init();
-    bytes32_t b = BYTES32_UINT(2);
-    assert(stack_push(&s, b_one));
-    assert(stack_push(&s, b));
+    STACK_UINT(s, 1);
+    STACK_UINT(s, 2);
+    assert(s.count == 2);
     ASSERT_BYTES32_UINT(s.b[1], 2);
     stack_free(s);
 
     s = stack_init();
     for(int i=0; i<STACK_MAX; i++)
-    {
-        b = BYTES32_UINT(i);
-        assert(stack_push(&s, b));
-    }
-    b = BYTES32_UINT(STACK_MAX);
+        STACK_UINT(s, i);
+    
+    bytes32_t b = BYTES32_UINT(STACK_MAX);
     assert(stack_push(&s, b) == false);
     assert(s.count == STACK_MAX);
     stack_free(s);
@@ -40,10 +40,10 @@ void test_pop()
     printf("\n\ttest pop\t\t");
     
     stack_t s = stack_init();
-    bytes32_t b = BYTES32_UINT(2);
-    assert(stack_push(&s, b_one));
-    assert(stack_push(&s, b));
+    STACK_UINT(s, 1);
+    STACK_UINT(s, 2);
     
+    bytes32_t b;
     assert(stack_pop(&b, &s));
     ASSERT_BYTES32_UINT(b, 2);
     assert(s.count == 1);
@@ -58,12 +58,80 @@ void test_pop()
     assert(stack_memory());
 }
 
+void test_dup()
+{
+    printf("\n\ttest dup\t\t");
+    
+    stack_t s = stack_init();
+    assert(stack_dup(&s, 1) == false);
+    stack_free(s);
+    
+    s = stack_init();
+    STACK_UINT(s, 1);
+    assert(stack_dup(&s, 1));
+    assert(s.count == 2);
+    ASSERT_BYTES32_UINT(s.b[1], 1);
+    stack_free(s);
+
+    s = stack_init();
+    STACK_UINT(s, 1);
+    assert(stack_dup(&s, 2) == false);
+    assert(s.count == 1);
+    stack_free(s);
+
+    s = stack_init();
+    STACK_UINT(s, 1);
+    STACK_UINT(s, 2);
+    assert(stack_dup(&s, 2));
+    assert(s.count == 3);
+    ASSERT_BYTES32_UINT(s.b[1], 2);
+    stack_free(s);
+    
+    assert(stack_memory());
+}
+
+void test_swap()
+{
+    printf("\n\ttest swap\t\t");
+    
+    stack_t s = stack_init();
+    assert(stack_swap(&s, 1) == false);
+    stack_free(s);
+    
+    s = stack_init();
+    STACK_UINT(s, 1);
+    assert(stack_swap(&s, 1) == false);
+    stack_free(s);
+    
+    s = stack_init();
+    STACK_UINT(s, 1);
+    STACK_UINT(s, 2);
+    assert(stack_swap(&s, 1));
+    ASSERT_BYTES32_UINT(s.b[0], 2);
+    ASSERT_BYTES32_UINT(s.b[1], 1);
+    stack_free(s);
+    
+    
+    s = stack_init();
+    STACK_UINT(s, 1);
+    STACK_UINT(s, 2);
+    STACK_UINT(s, 3);
+    assert(stack_swap(&s, 2));
+    ASSERT_BYTES32_UINT(s.b[0], 3);
+    ASSERT_BYTES32_UINT(s.b[2], 1);
+    stack_free(s);
+    
+    assert(stack_memory());
+}
+
 void test_stack()
 {
     printf("\ntest stack\t\t");
 
     test_push();
     test_pop();
+    test_dup();
+    test_swap();
 }
 
 
